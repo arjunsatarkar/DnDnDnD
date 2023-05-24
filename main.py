504cbcec63407d8b54ba14079a74fa48513e921f
Arjun Satarkar <me@arjunsatarkar.net>
2023-05-24 22:10:41 +0530

    Reformat

--- main.py
+++ main.py
@@ -2,20 +2,26 @@
 import sys
 
 try:
-	word_num = int(sys.argv[1])
+    word_num = int(sys.argv[1])
 except IndexError:
-	word_num = None
+    word_num = None
 
 WORDLIST_FILE_PATH = "corncob_lowercase.txt"
 
 with open(WORDLIST_FILE_PATH, "r") as wordlist_file:
-	valid_words = set([word[0].upper() + word.rstrip()[1:] for word in wordlist_file.readlines() if word.startswith("d")])
+    valid_words = set(
+        [
+            word[0].upper() + word.rstrip()[1:]
+            for word in wordlist_file.readlines()
+            if word.startswith("d")
+        ]
+    )
 
 sys.stdout.write("Dungeons & Dragons")
 for word in valid_words:
-	if word_num is not None:
-		if word_num <= 0:
-			break
-		word_num -= 1
-	sys.stdout.write(" & " + word)
+    if word_num is not None:
+        if word_num <= 0:
+            break
+        word_num -= 1
+    sys.stdout.write(" & " + word)
 print()
